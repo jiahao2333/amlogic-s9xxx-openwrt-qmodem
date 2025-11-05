8f831b3ad7ffd5ee15b59ae98ec40de4644314a3
jiahao2333 <1198408384@qq.com>
2025-11-04 19:19:50 -0800

    [ADD] passwall&ddns

--- config/lede_master/diy-part1.sh
+++ config/lede_master/diy-part1.sh
@@ -8,9 +8,15 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#qmodem
 echo 'src-git qmodem https://github.com/FUjr/QModem.git;main' >> feeds.conf.default
 ./scripts/feeds update qmodem
 ./scripts/feeds install -a -p qmodem
+#passwall
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default
+echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default
+#ddns-go
+git clone https://github.com/sirpdboy/luci-app-ddns-go package/ddns-go
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
 
